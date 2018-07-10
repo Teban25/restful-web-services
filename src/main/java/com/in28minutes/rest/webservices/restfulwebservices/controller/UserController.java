bf842445bfc3c14e7b5f4d0f495465528129ab03
Teban25 <exteban.1225@gmail.com>
2018-07-09 22:52:29 -0500

    Se agrega el servicio POST para agregar un usuario nuevo

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.rest.webservices.restfulwebservices.dao.UserDaoService;
 import com.in28minutes.rest.webservices.restfulwebservices.model.User;
@@ -24,6 +29,16 @@ public class UserController {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
